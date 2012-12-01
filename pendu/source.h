7831129cc4ea427347b78f76c886b7016b582bc5
Coucou <julien.deloor@gmail.com>
2012-12-01 18:53:53 +0100

    Ajout carte elec arenib

--- pendu/source.h
+++
@@ -1,6 +0,0 @@
-
-int taille_chaine(const char* t);
-void init(char * t,int Taille);
-char LireCaractere();
-int verification(char caractere,char * mot,int taille,char * Reponse);
-char* PIC_WORD();
